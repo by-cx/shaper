c6bdc4f9d7bdff350782cd9adeee95c0a4e5c382
Adam Strauch <cx@initd.cz>
2012-09-24 02:50:36 +0200

    Script parser

--- shaper-script.py
+++ shaper-script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import re
+import json
 
 
 def print_error(msg):
@@ -16,33 +17,43 @@ class ShaperScript(object):
             return [x.strip("\n") for x in f.readlines() if x.strip("\n")]
 
     def parse(self):
-        def line_parse(data, deep=0):
+        def line_parse2(data, index=0, deep=0):
             subtree = []
-            for i, rule in enumerate(data):
+            while True:
+                if index + 1 > len(data):
+                    break
                 item = {}
+                rule = data[index]
                 s = re.search("^([ ]*)", rule)
                 if s:
                     if len(s.group()) % 4 == 0:
                         if len(s.group()) / 4 == deep:
+                            #print index, rule
                             # TODO: check name=value pairs
-                            parsed_line = [(x.split("=")[0], x.split("=")[1]) for x in rule.split(" ") if rule.split("=") == 2]
-                            for x in parsed_line:
-                                # TODO: check right item name
-                                # TODO: check right item values
-                                item[x[0]] = x[1]
-                        else:
+                            for x in rule.split():
+                                key = x.split("=")[0]
+                                value = x.split("=")[1]
+                                item[key] = value
+                        elif len(s.group()) / 4 > deep:
                             # TODO: check right deep
-                            print "\n".join(data[i + 1:])
-                            subtree[-1]["subtree"] = line_parse(data[i + 1:], deep + 1)
+                            subindex, subtree[-1]["subtree"] = line_parse2(data, index, deep + 1)
+                            #print subindex
+                            index = subindex
+                            continue
+                        else:
+                            break
                     else:
                         print_error("You can use just %4 spaces (%s)" % rule)
-                subtree.append(item)
-            return subtree
+                if item:
+                    subtree.append(item)
+                index += 1
+            return index, subtree
 
         rules = self.load()
-        tree = line_parse(rules)
+        tree = line_parse2(rules)
         return tree
 
 if __name__ == "__main__":
     shaper_script = ShaperScript("shaper_script")
-    shaper_script.parse()
+    index, data = shaper_script.parse()
+    print json.dumps(data, indent=4)
